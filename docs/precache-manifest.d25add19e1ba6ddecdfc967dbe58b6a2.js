ed408270be460dbd18a0fad7fcb8e3dd85f3d03e
Amit Mundra <amit.mundra52@gmail.com>
2018-07-17 20:34:58 +0530

    second build

---
+++ docs/precache-manifest.d25add19e1ba6ddecdfc967dbe58b6a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "1505b11e795cf4f2d741",
+    "url": "/js/chunk-vendors.1505b11e.js"
+  },
+  {
+    "revision": "73e8602eed39357af3dd",
+    "url": "/js/app.73e8602e.js"
+  },
+  {
+    "revision": "b1994864b237f483b58bc52861eb2a92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73e8602eed39357af3dd",
+    "url": "/css/app.9de3b0af.css"
+  }
+];
