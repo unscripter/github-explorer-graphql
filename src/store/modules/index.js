a177e0326da099e0fe2b3988442fb3a706d45a17
Amit Mundra <amit.mundra52@gmail.com>
2018-07-17 19:21:16 +0530

    store module and constants

---
+++ src/store/modules/index.js
@@ -0,0 +1,5 @@
+import sidebar from './sidebar'
+
+export default {
+    sidebar
+}
---
+++ src/store/modules/sidebar.js
@@ -0,0 +1,23 @@
+import { SHOW_HIDE_NAVIGATION } from '../constants/constant'
+
+
+const state = {
+    isVisible: false,
+}
+
+const getters = {
+    visible: state => state.isVisible
+}
+
+const mutations = {
+    [SHOW_HIDE_NAVIGATION](state) {
+        debugger;
+        state.isVisible = !state.isVisible
+    }
+}
+
+export default {
+    state,
+    mutations,
+    getters
+}
