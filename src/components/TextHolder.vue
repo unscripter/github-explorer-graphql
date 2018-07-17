d969c07bab3e4b9ba88bf67dbe57ccd05ff7f04e
Amit Mundra <amit.mundra52@gmail.com>
2018-07-17 18:50:51 +0530

    Text holder for making heading and data binding

---
+++ src/components/TextHolder.vue
@@ -0,0 +1,49 @@
+<template lang="html">
+    <span
+        :class="{
+            'text-holder-wrapper': content === undefined,
+            'center': center
+            }"
+        :style="styleObj"
+    >
+        <div class="text-holder" v-if="!content"></div>
+        <slot></slot>
+    </span>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            content: ''
+        }
+    },
+    props: ['width', 'height', 'center'],
+    computed: {
+        styleObj() {
+            if (this.content) {
+                return {
+                    width: this.width,
+                    hetight: this.height
+                }
+            } else {
+                return null
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.text-holder-wrapper {
+    display: block;
+    transform: translate3d(0, 0, 0);
+    &.center {
+        margin: 0 auto;
+    }
+    .text-holder {
+        background-color: rgba(136, 136, 136, 0.2);
+        height: 100%;
+    }
+}
+</style>
