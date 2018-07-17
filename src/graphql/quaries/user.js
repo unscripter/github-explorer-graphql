2c5927ec56d8e644b77f664844d75498ee98a975
Amit Mundra <amit.mundra52@gmail.com>
2018-07-17 19:25:55 +0530

    quaries created for gettign search data and other user info with repos

---
+++ src/graphql/quaries/user.js
@@ -0,0 +1,142 @@
+import gql from 'graphql-tag'
+
+export const searchUserDetails = 
+    gql`query User($login: String!){
+      user(login: $login) {
+        name
+          login
+        bio 
+          avatarUrl
+            bioHTML
+      followers{
+        totalCount
+      }
+      following{
+        totalCount
+      }
+      }
+    } `
+
+export const fetchRepoDetails = 
+gql`query Search($username: String!) {
+  search(first:1, query:$username, type:USER){
+    nodes {
+      ... on User {
+        repositories(first: 5){
+    edges {
+      cursor
+      node {
+        name
+      description
+      watchers {
+        totalCount
+      }
+      forks {
+        totalCount
+      }
+      stargazers {
+        totalCount
+      }
+      languages(last: 3) {
+        edges {
+          node {
+            name
+          }
+        }
+      }
+      updatedAt
+      }
+    }
+  }
+      }
+    }
+  }
+}`
+
+export const fetchPreviousRepoDetails = 
+gql`query Search($username: String! $before: String!) {
+  search(first: 5 query: $username type: USER){
+    nodes {
+      ... on User {
+        repositories(last: 5, before: $before){
+    edges {
+      cursor
+      node {
+        name
+      description
+      watchers {
+        totalCount
+      }
+      forks {
+        totalCount
+      }
+      stargazers {
+        totalCount
+      }
+      languages(last: 3) {
+        edges {
+          node {
+            name
+          }
+        }
+      }
+      updatedAt
+      }
+    }
+  }
+      }
+    }
+  }
+}`
+
+  export const fetchNextRepoDetails = 
+  gql`query Search($username: String! $after: String!) {
+    search(first: 1 query: $username type: USER){
+      nodes {
+        ... on User {
+          repositories(first: 5 after: $after){
+      edges {
+        cursor
+        node {
+          name
+        description
+        watchers {
+          totalCount
+        }
+        forks {
+          totalCount
+        }
+        stargazers {
+          totalCount
+        }
+        languages(last: 3) {
+          edges {
+            node {
+              name
+            }
+          }
+        }
+        updatedAt
+        }
+      }
+    }
+        }
+      }
+    }
+  }`
+  
+    export const searchAnonymousUser = 
+    gql`query search($query: String!){
+      search(first: 15, query: $query, type: USER) {
+        edges {
+          node {
+            ... on User {
+              login
+              avatarUrl
+              name
+            }
+          }
+        }
+      }
+    }`
+
