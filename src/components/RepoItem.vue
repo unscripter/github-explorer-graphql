4d1e996b90ec7d76e07d21d4d0d6930699fb21d4
Amit Mundra <amit.mundra52@gmail.com>
2018-07-17 18:55:24 +0530

    Repo item for transition list

---
+++ src/components/RepoItem.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+    <a class="repo-item">
+      <repo-content :repo="repo"></repo-content>      
+    </a>    
+</div>
+</template>
+
+<script>
+import RepoContent from './RepoContent'
+
+export default {
+    components: {
+        RepoContent
+    },
+    props: ['repo']
+    
+}
+</script>
+
+<style lang="scss" scoped>
+.repo-item {
+    border-radius: 6px;
+    background-color: #ffffff;
+    border: solid 0.5px #e5e5e5;
+    margin-bottom: 5px;
+    display: block;
+    text-decoration: none !important;
+    overflow: hidden;
+}
+</style>
+
+
