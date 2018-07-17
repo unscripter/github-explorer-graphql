82768158af9d6ced93700bae7eee4d70b1a49bf6
Amit Mundra <amit.mundra52@gmail.com>
2018-07-17 19:03:27 +0530

    Search Component created

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="row search">
+        <input @focus="$emit('focus')"
+            @input="handleInput"
+            @keyup.enter="$emit('search')"
+            aria-label="Search"
+            type="search" 
+            :placeholder="placeholder" 
+            class="search-input">
+        <button @click="$emit('search')" class="search-button"><i class="fa fa-search"></i></button>
+    </div>
+</template>
+<script>
+    export default {
+        props: ['placeholder', 'searchText'],
+        methods: {
+            handleInput(e) {
+                const { value } = e.target;
+                this.$emit('input', value);
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+.search {
+    grid-template-columns: 200px 40px;
+    border: 1px solid black;
+    border-radius: 10px;
+    margin: 1px;
+    // @media (max-width: 768px) {
+    //    grid-template-columns: 270px 40px;
+    // }
+    .search-input {
+        border: 1px solid black;
+        border-radius: 10px;
+        padding: 10px;
+        width: 220px;
+        margin-right: 5px;
+    //     @media (max-width: 768px) {
+    //     width: 290px;
+    // }
+    }
+    .search-button {
+        height: 40px;
+        margin-left: 10px;
+        border-radius: 8px;
+        background-image: linear-gradient(to bottom, #a5de41, #55b130);
+        box-shadow: inset 0 0.5px 0.5px 0 #e2ffce;
+        border: none;
+        font-family: GothamPro-Bold;
+        font-size: 14px;
+        letter-spacing: 0.5px;
+        // text-align: center;
+        padding: 20px 20px 20px 20px;
+
+        &.icon {
+            padding: 10px;
+            width: 44px;
+            font-size: 20px;
+        }
+    }
+}
+</style>
+
