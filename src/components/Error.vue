93643f7b5d085840bddeeb2e449adb473f0f49f1
Amit Mundra <amit.mundra52@gmail.com>
2018-07-23 22:57:56 +0530

    loading, error and some new view components added

---
+++ src/components/Error.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Something went wrong</h1>
+    </div>
+</template>
---
+++ src/components/Loading.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="loading" v-if="$apollo.loading">
+        <div class="loading"></div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.loading {
+        border-radius: 50%;
+        width: 24px;
+        height: 24px;
+        border: .25rem solid black;
+        border-top-color: white;
+        animation: spin 1s infinite linear;
+        margin: 15px auto;
+}
+</style>
+
